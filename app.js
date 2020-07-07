8aa691d3e92a593c5419aa81109baa61f496c1ec
mihalichpalich <pavlovtfspb@gmail.com>
2020-07-07 16:30:54 +0300

    created first middleware-function

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {graphqlHTTP} = require('express-graphql');
+const {buildSchema} = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-    res.send("Hello World!");
-});
+app.use('/graphql', graphqlHTTP({
+    //events: [String!]! возвращает массив не null из строк не null
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+        
+        type RootMutation {
+            createEvent(name: String): String
+        }
+    
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => { //имя resolve должно совпадать с именем типа
+            return ['Romantic Cooking', 'Sailing', 'All-night Coding'];
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName; //вернет тип mutation и значение параметра args - "createEvent": "Sports"
+        }
+    },
+    graphiql: true
+}));
 
 app.listen(process.env.PORT || 3000, function (err) {
     if (err) {
