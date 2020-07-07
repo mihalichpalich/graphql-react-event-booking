c302371aa37370f6e9bd444c299f06bc0daa548e
mihalichpalich <pavlovtfspb@gmail.com>
2020-07-07 19:12:05 +0300

    made events creation

--- app.js
+++ app.js
@@ -5,17 +5,37 @@ const {buildSchema} = require('graphql');
 
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json());
 
 app.use('/graphql', graphqlHTTP({
-    //events: [String!]! возвращает массив не null из строк не null
+    //events: [Event!]! возвращает массив не null из объектов не null
+    //input - объект, объединяющий несколько типов
+    //ID - специальный тип
+    //типа Date не существует
     schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+        
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!            
+        }
+    
         type RootQuery {
-            events: [String!]!
+            events: [Event!]!
         }
         
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
     
         schema {
@@ -25,11 +45,18 @@ app.use('/graphql', graphqlHTTP({
     `),
     rootValue: {
         events: () => { //имя resolve должно совпадать с именем типа
-            return ['Romantic Cooking', 'Sailing', 'All-night Coding'];
+            return events;
         },
         createEvent: (args) => {
-            const eventName = args.name;
-            return eventName; //вернет тип mutation и значение параметра args - "createEvent": "Sports"
+            const event = {
+                _id: Math.random().toString(),
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price, //"+" переводит строку в число
+                date: args.eventInput.date
+            };
+            events.push(event);
+            return event;
         }
     },
     graphiql: true
