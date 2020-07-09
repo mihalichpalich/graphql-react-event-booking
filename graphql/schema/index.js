9f7ed56e58910f4e82258c79119fb64c23db78bd
mihalichpalich <pavlovtfspb@gmail.com>
2020-07-10 02:49:22 +0300

    added booking creation and removing

--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -5,6 +5,14 @@ const {buildSchema} = require('graphql');
 //ID - специальный тип
 //типа Date не существует
 module.exports = buildSchema(`
+        type Booking {
+            _id: ID!
+            event: Event!
+            user: User!
+            createdAt: String!
+            updatedAt: String!
+        }
+
         type Event {
             _id: ID!
             title: String!
@@ -35,11 +43,14 @@ module.exports = buildSchema(`
     
         type RootQuery {
             events: [Event!]!
+            bookings: [Booking!]!
         }
         
         type RootMutation {
             createEvent(eventInput: EventInput): Event
             createUser(userInput: UserInput): User
+            bookEvent(eventId: ID!): Booking!
+            cancelBooking(bookingId: ID!): Event!
         }
     
         schema {
